8a88db04b8785fcf4bc86b63134fe5b5d506ad46
Thabith123 <It24610812@my.sliit.lk>
2024-11-07 11:46:11 +0530

    Update IT24610812Lab3Q1.java

--- IT24610812Lab3Q1.java
+++ IT24610812Lab3Q1.java
@@ -4,15 +4,12 @@ public class IT24610812Lab3Q1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Input price per kg
         System.out.print("Enter the price of 1 kg of rice: ");
         double pricePerKg = scanner.nextDouble(); // e.g., 100
         
-        // Input quantity
         System.out.print("Enter the number of kilograms you want to buy: ");
         double quantity = scanner.nextDouble(); // e.g., 4
         
-        // Calculate total amount
         double totalAmount = pricePerKg * quantity;
         
         // Output total amount
